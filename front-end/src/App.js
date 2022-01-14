179264e2e55c54c494c44d86ae21e96412a6f325
Spencer Soto <spencer.soto@gmail.com>
2022-01-14 14:37:27 -0500

    grid view

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 const App = () => {
+  const [characters, setCharacters] = useState([
+      { id: 1, name: 'Rick Sanchez' , status: 'Alive' , image: "https://rickandmortyapi.com/api/character/avatar/1.jpeg" }
+  ]);
+
+ 
+
+    useEffect(() => {
+      fetch('https://rickandmortyapi.com/api/character')
+      .then(res => res.json())
+      .then(data => setCharacters(data.results))
+    }, []); // run once on mount 
+
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
@@ -12,6 +24,19 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <h2 className="character-list">
+
+      {characters.map((character)=> (
+        <div key={character.id}>
+          {character.name}
+          {/* {character.image} */}
+          {character.status}
+          {character.gender}
+          
+          </div>
+      ))}
+     </h2> 
+       
     </div>
   );
 }
