5eb4243198d709d73565c1f94b377e1a453f91cc
Spencer Soto <spencer.soto@gmail.com>
2022-01-17 08:40:18 -0500

    character component

--- front-end/src/components/Character.js
+++ front-end/src/components/Character.js
@@ -1,1 +1,30 @@
 // Write your Character component here
+import React from 'react';
+// import "./componen/Character.css";
+
+class Character extends React.Component {
+    state = {
+      user: null,
+      error: null,
+      loading: true,
+    };
+    componentDidMount() {
+      fetch('https://rickandmortyapi.com/api/character')
+        .then(response => response.json())
+        .then(data => {
+          const user = data.results[0];
+          this.setState({ user, loading: false });
+        })
+        .catch(error => this.setState({
+          error: error.message,
+          loading: false,
+        }));
+    }
+    render() {
+      return (
+        <section>
+          {this.props.render(this.state)}
+        </section>
+      );
+    }
+  } 
