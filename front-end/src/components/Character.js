aa151ea1bfe1ba8474eb0156fad6be414cbda1b4
Spencer Soto <spencer.soto@gmail.com>
2022-01-17 09:36:12 -0500

    working on single character component

--- front-end/src/components/Character.js
+++ front-end/src/components/Character.js
@@ -1,26 +1,44 @@
 // Write your Character component here
+import React, { useEffect, useState } from 'react';
 import React from 'react';
 // import "./component/Character.css";
 
-const CharacterCard = (props) => {
+const CharacterCard = () => {
+    const [characters, setCharacters] = useState([
+        { id: 1, name: 'Rick Sanchez' , status: 'Alive' , image: "https://rickandmortyapi.com/api/character/avatar/1.jpeg" }
+    ]);
+  
+  
+      useEffect(() => {
+        fetch('https://rickandmortyapi.com/api/character/2')
+        .then(res => res.json())
+        .then(data => setCharacters(data.results))
+      }, []); // run once on mount 
+  
+  
+  
     return (
-      <article className="character__card">
-        <img
-          src={props.character.image}
-          alt={props.character.name}
-          title={props.character.name}
-          className="character__card--img"
-        />
-        <h2 className="character__card--name">{props.character.name}</h2>
-        <p className="character__card--species">{props.character.species}</p>
-        {/* <Link>
-          to={`/character-detail/${props.character.id}`}
-          className="character__card--link"
-          title="Go to character detail"
-        >
-          More info
-        </Link> */}
-      </article>
+      <div className="App">
+        <h1 className="Header">Single Character</h1>
+        <h2 className="character">
+          
+  
+        {characters.map((character)=> (
+          <div key={character.id}>
+           <p> {character.name}</p>
+           <img src={character.image} alt="profile pic" />
+            {/* {character.image} */}
+             {character.status}
+             {/* {character.gender} */}
+            
+            </div>
+        ))}
+       </h2> 
+         
+      </div>
+      
     );
-  };
+  }
+  
+  
   export default CharacterCard;
